582545ab9e1fa2fca4c56026e867d593e15ddf0c
Jianhua Zhen <jianhuazhen213@gmail.com>
2024-10-26 18:02:30 -0400

    Started working on profile page

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
-import logo from '../styles/Capture.png';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <img src={logo} alt="Capture" className="capture-image" />;
+      <img src="/logo.png" alt="logo" className="logo-image" />;
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/About">About</Link></li>
+        <li><Link to="/Profile">Profile</Link></li>
         <li><Link to="/modules">Modules</Link></li>
       </ul>
     </nav>
