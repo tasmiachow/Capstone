0af1057a3e22b1de1d55e5d2209ab160c4105d89
Jianhua Zhen <jianhuazhen213@gmail.com>
2024-10-25 22:23:05 -0400

    Added background and changed logo

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Home.css';
+import learningGif from '../styles/handigif.gif';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="text-container">
+        <h1>Dive into ASL and express Yourself Beyond Words</h1>
+        <p>
+          How? Allow your camera, start working on the lessons, and make ASL gestures!
+        </p>
+        <Link to="/modules">
+          <button className="start-button">Get started</button>
+        </Link>
+      </div>
+      <div className="gif-container">
+        <img src={learningGif} alt="Learning GIF" className="learning-gif" />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
