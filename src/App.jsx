538e2cd2a06274e13ece0cb9acd3c4f2fe7928c1
tasmiachow <tasmiachowdhury123@gmail.com>
2024-10-27 22:32:14 -0400

    login style

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import SlidingLoginForm from './components/SlidingLoginForm';
+
+function App() {
+  return (
+    <div className="App">
+      <SlidingLoginForm />
+    </div>
+  );
+}
+
+export default App;
+
