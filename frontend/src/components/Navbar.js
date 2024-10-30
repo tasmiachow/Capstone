a28a2b736936f057370658adb94e8787e5d58d86
samicbeans <sami.chen28@myhunter.cuny.edu>
2024-10-30 00:28:46 -0400

    replaced navbar text & edit profile button's text  with MaterialUI icons

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
+import HomeIcon from '@mui/icons-material/Home';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import InfoIcon from '@mui/icons-material/Info';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <img src="/logo.png" alt="logo" className="logo-image" />;
+      <img src="/logo.png" alt="logo" className="logo-image" />
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/About">About</Link></li>
-        <li><Link to="/Profile">Profile</Link></li>
-        <li><Link to="/modules">Modules</Link></li>
-        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/"><div className='navitem'><HomeIcon fontSize='medium'/></div></Link></li>
+        <li><Link to="/About"><div className='navitem'><InfoIcon fontSize='small'/></div></Link></li>
+        <li><Link to="/Profile"><div className='navitem'><AccountCircleIcon fontSize='small'/></div></Link></li>
+        <li><Link to="/modules"><div className='navitem'><MenuBookIcon fontSize='small'/></div></Link></li>
+        <li><Link to="/login"><div className='navitem'>Login</div></Link></li>
       </ul>
     </nav>
   );
