bbeca9a28737b057e419f0da502bf8df79fb60df
Jianhua Zhen <jianhuazhen213@gmail.com>
2024-10-30 20:23:20 -0400

    ready for merge

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
-import HomeIcon from '@mui/icons-material/Home';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import InfoIcon from '@mui/icons-material/Info';
-import MenuBookIcon from '@mui/icons-material/MenuBook';
 
+{/* <li><Link to="/"><div className='navitem'><HomeIcon fontSize='medium'/></div></Link></li>
+<li><Link to="/About"><div className='navitem'><InfoIcon fontSize='small'/></div></Link></li>
+<li><Link to="/modules"><div className='navitem'><MenuBookIcon fontSize='small'/></div></Link></li>
+<li><Link to="/Profile"><div className='navitem'><AccountCircleIcon fontSize='small'/></div></Link></li> */}
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <img src="/logo.png" alt="logo" className="logo-image" />
+      <img src="/logo.png" alt="logo" className="logo-image" />;
       <ul>
-        <li><Link to="/"><div className='navitem'><HomeIcon fontSize='medium'/></div></Link></li>
-        <li><Link to="/About"><div className='navitem'><InfoIcon fontSize='small'/></div></Link></li>
-        <li><Link to="/modules"><div className='navitem'><MenuBookIcon fontSize='small'/></div></Link></li>
-        <li><Link to="/Profile"><div className='navitem'><AccountCircleIcon fontSize='small'/></div></Link></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/About">About</Link></li>
+        <li><Link to="/Profile">Profile</Link></li>
+        <li><Link to="/modules">Modules</Link></li>
         <li><Link to="/login"><div className="navitem login-button">Login</div></Link></li>
       </ul>
     </nav>
