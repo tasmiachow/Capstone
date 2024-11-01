1e17070ae6fede5c538a9b93d2ab1ca428859fab
unknown <brina9169@gmail.com>
2024-11-01 19:38:14 -0400

    changed file

--- Capstone_Midterm_Model/myproject/myproject/settings.py
+++ Capstone_Midterm_Model/myproject/myproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1vdo296fu8=o2frnpb9qcv-&6!gd35@#zcqazc*9*43r^dh5@*'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
